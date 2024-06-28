d7c9fb0faaed0b3aff1798bb12ef6175e10a1efa
Sixtofuken <02jc02@gmail.com>
2024-06-29 01:48:02 +0800

    Update qbc.lua

--- qbc.lua
+++ qbc.lua
@@ -1,4 +1,9 @@
-A1=0x5BA3730
-A2=0x5b24454
-A3=0x5b24458
-LIST_KEYS = {"UP[644"}
+A1 = 0x5BA3730
+A2 = 0x5B24454
+A3 = 0x5B24458
+
+LIST_KEYS = {
+    "UP[644]",
+    "AnotherEncodedString",
+    "ThirdEncodedString"
+}
