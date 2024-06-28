1e674c9bcab08e4249783974479676487f175723
Sixtofuken <02jc02@gmail.com>
2024-06-29 00:40:26 +0800

    Update qbc.lua

--- qbc.lua
+++ qbc.lua
@@ -1,4 +1,9 @@
-A1=0x5BA3730
-A2=0x5b24454
-A3=0x5b24458
-valid_models = { "RMX3151", "MODEL2", "MODEL3" }
+<?php
+// Assuming you want to store these values in PHP variables
+$A1 = 0x5BA3730;
+$A2 = 0x5b24454;
+$A3 = 0x5b24458;
+
+// Valid models array remains the same
+$valid_models = ["RMX3151", "MODEL2", "MODEL3"];
+?>
