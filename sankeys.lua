94f3440dd7ee49a935274417a1b73c4e6ff90c8e
Sixtofuken <02jc02@gmail.com>
2024-07-04 01:18:41 +0800

    Update sankeys.lua

--- sankeys.lua
+++ sankeys.lua
@@ -1,11 +1,3 @@
 return {
-    { key = "Lqilql{#[9:3", owner = "Zion", expiration = "2024:9:2" },
     { key = "UP[6484", owner = "Sixto Cpm", expiration = "3932:09:27" },
-    { key = "FSK4<3<", owner = "Dior Cpm", expiration = "2024:9:2" },
-    { key = "VP0J<83I", owner = "VK Garage", expiration = "2024:9:2" },
-    { key = "Y5538", owner = "Benjamin", expiration = "2024:9:2" },
-    { key = "543:446VJ", owner = "Leo Niel", expiration = "2024:9:2" },
-    { key = "Y5365", owner = "Kaito Works", expiration = "2024:9:2" },
-    { key = "FSK556:", owner = "Mango GG", expiration = "2024:11:13" },
-    { key = "Lqilql{#[9<8", owner = "Yu Wa", expiration = "2024:7:2" }
 }
